2808591512e9c00e4cd522578d66266bad871a33
Dzemaludin_Oraovcanin <utko1998.do@gmail.com>
2023-03-27 13:52:24 +0300

    .

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,22 @@
 // 1.Yöntem => React - CDN (eksik araçlar)
 // 2.Yöntem => create-react-app
 
-var product = {
+var products = [{
   name: "Ipone 15",
   price: 4000,
   description: "good phone",
-  colors: ["silver", "red", "white"]
-};
+  colors: ["silver", "red", "white", "black"]
+}, {
+  name: "Ipone 16",
+  price: 6000,
+  description: "good phone",
+  colors: ["silver", "red", "white", "black"]
+}, {
+  name: "Ipone 17",
+  price: 7000,
+  description: "good phone",
+  colors: ["silver", "red", "black"]
+}];
 function formatPrice(product) {
   return /*#__PURE__*/React.createElement("p", null, product.price, " TL");
 }
@@ -19,8 +29,15 @@ function printDescription(product) {
 }
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
-}, "Lista Proizvoda"), /*#__PURE__*/React.createElement("div", {
-  id: "product-details"
-}, product.name && product.name.length > 3 ? /*#__PURE__*/React.createElement("h2", null, " ", product.name, " ") : /*#__PURE__*/React.createElement("p", null, "The product name has not been entered."), product.price && product.price > 20 && formatPrice(product), product.description && /*#__PURE__*/React.createElement("p", null, " ", product.description, " "), product.colors.length > 1 ? /*#__PURE__*/React.createElement("p", null, " you can chose the color") : "", /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."))));
+}, "Lista Proizvoda"), products.map(function (product, index) {
+  return /*#__PURE__*/React.createElement("div", {
+    id: "product-details",
+    key: index
+  }, product.name && product.name.length > 3 ? /*#__PURE__*/React.createElement("h2", null, " ", product.name, " ") : /*#__PURE__*/React.createElement("p", null, "The product name has not been entered."), product.price && product.price > 20 && formatPrice(product), product.description && /*#__PURE__*/React.createElement("p", null, " ", product.description, " "), product.colors.length > 1 ? /*#__PURE__*/React.createElement("p", null, " you can chose the color") : "", /*#__PURE__*/React.createElement("ul", null, product.colors.map(function (color, index) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: index
+    }, "  ", color, " ");
+  })));
+}));
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(template);
--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
 // 1.Yöntem => React - CDN (eksik araçlar)
 // 2.Yöntem => create-react-app
 
-let product = {
+let products = [
+{
   name: "Ipone 15",
   price: 4000,
   description: "good phone",
-  colors:["silver","red","white"]
-};
+  colors: ["silver","red","white","black"]
+},
+{
+  name: "Ipone 16",
+  price: 6000,
+  description: "good phone",
+  colors: ["silver","red","white","black"]
+},
+{
+  name: "Ipone 17",
+  price: 7000,
+  description: "good phone",
+  colors: ["silver","red","black"]
+}
+
+
+]
+
 function formatPrice(product) {
   return <p>{product.price} TL</p>;
 }
@@ -19,18 +36,26 @@ function printDescription(product) {
 let template = (
   <div>
     <h1 id="header">Lista Proizvoda</h1>
-    <div id="product-details">
-      {(product.name && product.name.length > 3) ?<h2> {product.name} </h2>: <p>The product name has not been entered.</p>}
-      { (product.price && product.price > 20 ) && formatPrice(product)}
-      {/* {printDescription(product)} */}
-      {product.description && <p> {product.description} </p>}
-      {product.colors.length > 1 ? <p> you can chose the color</p>:""}
-      <ul>
-        <li>Lorem, ipsum.</li>
-        <li>Lorem, ipsum.</li>
-        <li>Lorem, ipsum.</li>
-      </ul>
-    </div>
+
+    {
+      products.map((product,index) => (
+        <div id="product-details" key={index}>
+        {(product.name && product.name.length > 3) ?<h2> {product.name} </h2>: <p>The product name has not been entered.</p>}
+        { (product.price && product.price > 20 ) && formatPrice(product)}
+        {/* {printDescription(product)} */}
+        {product.description && <p> {product.description} </p>}
+        {product.colors.length > 1 ? <p> you can chose the color</p>:""}
+        
+        <ul>
+        {
+          product.colors.map((color,index) =>  (<li key={index}>  { color } </li>))
+        
+        }
+        </ul>
+      </div>
+      ))
+    }
+ 
   </div>
 );
 
